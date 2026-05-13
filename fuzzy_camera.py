9905c76cf5a85cc818d0e7c7e8c1454b53d0f175
megahomyak <g.megahomyak@gmail.com>
2026-05-13 19:19:43 +0500

    Absolute lightness bug fix

--- fuzzy_camera.py
+++ fuzzy_camera.py
@@ -4,7 +4,7 @@ cap = cv2.VideoCapture(0)
 while window(timeout=20)[0] is not None:
     frame = cap.read()[1]
     lightness = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    activation_mask = lightness > np.random.randint(0, 255, size=lightness.shape, dtype=np.uint8)
+    activation_mask = lightness > np.random.randint(0, 254, size=lightness.shape, dtype=np.uint8)
     frame[:] = 0
     frame[activation_mask] = 255
     window['image'](data=cv2.imencode('.png', frame)[1].tobytes())
