7a0566fa18f37d16aeebce63c8798e7714495f94
megahomyak <g.megahomyak@gmail.com>
2026-05-13 19:12:14 +0500

    Made this thing

---
+++ README.md
@@ -0,0 +1,14 @@
+# fuzzy camera
+
+a camera app that turns pixels into black or white randomly depending on how light they are (white are always white, black are always black, grays are 50/50 black/white)
+
+## running
+
+```
+poetry install --no-root
+poetry run python fuzzy_camera.py
+```
+
+## stopping
+
+ctrl + C
---
+++ fuzzy_camera.py
@@ -0,0 +1,10 @@
+import cv2, PySimpleGUI as sg, numpy as np
+window = sg.Window('fuzzy camera', [[sg.Image(filename='', key='image')]], size=(800, 800), resizable=True)
+cap = cv2.VideoCapture(0)
+while window(timeout=20)[0] is not None:
+    frame = cap.read()[1]
+    lightness = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    activation_mask = lightness > np.random.randint(0, 255, size=lightness.shape)
+    frame[:] = 0
+    frame[activation_mask] = 255
+    window['image'](data=cv2.imencode('.png', frame)[1].tobytes())
